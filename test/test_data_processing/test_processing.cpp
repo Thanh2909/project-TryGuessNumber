c8b584ce250212bb4f528c3573b90331e0b18bf9
Thanh2909 <tonynguyen29199@gmail.com>
2024-10-12 23:33:56 +0700

    Update test_processing.cpp

--- test/test_data_processing/test_processing.cpp
+++ test/test_data_processing/test_processing.cpp
@@ -27,28 +27,3 @@ TEST(GameTest, CorrectGuessOnFirstTry) {
     ASSERT_EQ(GameLibrary::guess_attempts[0], 1);
 }
 
-// TEST(GameTest, CorrectGuessAfterMultipleTries) {
-//     Playerdata player("John Doe", 25, "johnny", "Guessing Game", 0, 0);
-//     MockGame game({25, 40, 50}); // Giả sử số đoán chính xác sau 3 lần đoán
-//     game.start_game(player);
-//     ASSERT_EQ(GameLibrary::guess_attempts.size(), 1);
-//     ASSERT_EQ(GameLibrary::guess_attempts[0], 3);
-// }
-
-// TEST(GameTest, RecordGuessAttemptCalled) {
-//     Playerdata player("John Doe", 25, "johnny", "Guessing Game", 0, 0);
-//     MockGame game({50});
-//     game.start_game(player);
-//     ASSERT_FALSE(GameLibrary::guess_attempts.empty());
-// }
-
-// TEST(GameTest, AverageAttemptsCalculatedCorrectly) {
-//     Playerdata player("John Doe", 25, "johnny", "Guessing Game", 0, 0);
-//     MockGame game1({25, 40, 50});
-//     game1.start_game(player);
-//     MockGame game2({30, 50});
-//     game2.start_game(player);
-//     double avg_attempts = GameLibrary::get_average_attempts();
-//     ASSERT_NEAR(avg_attempts, 2.0, 0.001); // Tolerance for floating-point comparison
-// }
-
